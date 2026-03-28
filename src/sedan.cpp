25c536f175e123ec8986dfb1a337001781ffb84a
Daniel Frost Svenningsen <rarinbaton25@gmail.com>
2026-03-28 12:54:41 +0100

    completed

--- src/sedan.cpp
+++ src/sedan.cpp
@@ -9,28 +9,37 @@ Sedan::Sedan() : current_gear(0)
 
 void Sedan::shift_up()
 {
+    // if there's room to shift up, we do, else silently fail
+    if (this->get_current_gear_ratio() < gear_ratios.size()-1) this->current_gear++;
 }
 
 void Sedan::shift_down()
 {
+    // if there's room to shift down, we do, else silently fail
+    if (this->get_current_gear_ratio() > 0) this->current_gear--;
 }
 
 double Sedan::get_torque(double engine_rpm, double throttle)
 {
-    return 0;
+    // just returns 100 Nm times the throttle ratio
+    // independent of engine_rpm
+    return 1950 * throttle;
 }
 
 double Sedan::get_weight()
 {
-    return 1;
+    // hardcoded value
+    return 2000;
 }
 
 double Sedan::get_wheel_radius()
 {
-    return 1;
+    // hardcoded value
+    return 0.3;
 }
 
 double Sedan::get_current_gear_ratio()
 {
-    return 1;
+    // just return the local variable of the object
+    return gear_ratios[this->current_gear];
 }
